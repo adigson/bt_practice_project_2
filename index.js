c4d2959bdf32cbdbfe03411d5b8bb2900dc8d5c3
Musibau Bolaji <bolajimusibau20@gmail.com>
2026-09-16 15:19:28 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import { add, subtract } from "./math.js"; // Import math.js
+
+const math = require('./math.js'); // Import math.js
+
+console.log(math.subtract(10, 6)); // Call subtract function and log result
